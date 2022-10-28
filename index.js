36b6b5b6a9baca7e1024b1436d27c3007ea84195
MichaelaRasovska <michaela.rasovska@gmail.com>
2022-10-28 21:18:39 +0200

    vyřešeno

--- index.js
+++ index.js
@@ -2,40 +2,32 @@ console.log('funguju!');
 
 import { Task } from './Task/index.js';
 
-const renderTasks = () => {
-  fetch('https://apps.kodim.cz/daweb/trening-api/apis/tasks-api/tasks')
-    .then((response) => response.json())
-    .then(
-      (data) =>
-        (document.querySelector('.todo__tasks').innerHTML = data
-          .map((item) => Task(item))
-          .join('')),
-    );
-};
+fetch('https://apps.kodim.cz/daweb/trening-api/apis/tasks-api/tasks')
+  .then((response) => response.json())
+  .then(
+    (data) =>
+      (document.querySelector('.todo__tasks').innerHTML = data
+        .map((item) => Task(item))
+        .join('')),
+  );
 
-const renderUndoneTasks = () => {
-  fetch(
-    'https://apps.kodim.cz/daweb/trening-api/apis/tasks-api/tasks?done=false',
-  )
-    .then((response) => response.json())
-    .then(
-      (data) =>
-        (document.querySelector('.todo__tasks').innerHTML = data
-          .map((item) => Task(item))
-          .join('')),
-    );
-};
 let selected = false;
 
-
-document.querySelector('#checkbox-undone').addEventListener('click', () => {
-  selected = !selected;
-  console.log(selected);
-  if (selected === true) {
-    renderUndoneTasks();
-  } else {
-    renderTasks;
-  }
-});
-
-renderTasks();
+document
+  .querySelector('#checkbox-undone')
+  .addEventListener('click', (event) => {
+    event.preventDefault;
+    selected = !selected;
+    fetch(
+      `https://apps.kodim.cz/daweb/trening-api/apis/tasks-api/tasks${
+        selected === true ? '?done=false' : ''
+      }`,
+    )
+      .then((response) => response.json())
+      .then(
+        (data) =>
+          (document.querySelector('.todo__tasks').innerHTML = data
+            .map((item) => Task(item))
+            .join('')),
+      );
+  });
